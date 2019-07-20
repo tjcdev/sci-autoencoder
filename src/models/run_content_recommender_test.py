d50081aa0997c7956cfbd0e5a185fff22a2b5a24
tjcdev <TJCARTWRIGHT91@GMAIL.COM>
2019-07-20 12:36:01 +0100

    complete file for running the content recommender tests

--- src/models/run_content_recommender_test.py
+++ src/models/run_content_recommender_test.py
@@ -10,6 +10,7 @@ import math
 from scipy import sparse
 
 import sys
+import os 
 
 from evaluate import evaluate
 from content_recommender import Recommender
@@ -47,77 +48,30 @@ test = sparse.load_npz("test.npz")
 recommender = Recommender()
 similarity_matrix = recommender.similarity(embeddings)
 
-for i in range(1, train.shape[0]):
+fileName = 'results-' + str(embedding_size) + '_' + '.json'
+f = open(fileName,"w+")
+# Clear the current contents of the file
+f.truncate(0)
+f.write('{ "results": [')
+for i in range(1, 10): #train.shape[0]):
+    # Get the top projects that were predicted
     top_projects = recommender.top_projects(train.getrow(i), test.getrow(i), similarity_matrix, k)
 
+    # Generate our predictions and true values
     y_true, y_pred = recommender.generate_y(test.getrow(i), top_projects)
 
     # Evaluate our model
     precision, recall, refined_precision = evaluate(y_true, y_pred, top_projects, similarity_matrix)
 
-    print(precision)
-    print(recall)
-    print(refined_precision)
+    y_pred_string = '[' + ', '.join(y_pred.astype('str')) + ']'
+    y_true_string = '[' + ', '.join(y_true.astype('str')) + ']'
+    # Write the results to a JSON file
+    f.write('{ "user_index": %s, "precision": %s, "recall": %s, "refined_precision": %s, "y_pred": %s, "y_true": %s },' % (str(i), str(precision), str(recall), str(refined_precision), y_pred_string, y_true_string))
 
-'''
-# Load all the data we need
-users_projects_list = pd.read_pickle('data/processed/profile_projects_time_consistent')
-projects = pd.read_pickle('data/processed/project_data')
-users_projects_matrix =  pd.read_pickle('data/processed/active_profile_projects')
-similarity_matrix = pd.read_pickle('data/processed/similarity_matrix_'+str(embedding_size))
+# Delete the last trailing comma
+f.seek(f.tell() - 1, os.SEEK_SET)
+f.write('')
 
-max_sim = np.max(np.max(similarity_matrix))
-
-# Normalise the similarity
-similarity_matrix = (similarity_matrix + 1) / 2
-
-max_sim = np.max(np.max(similarity_matrix))
-
-# Setup our recommender
-rec = Recommender(projects, users_projects_matrix)
-
-precisions = []
-recalls = []
-refined_precisions = []
-
-
-num_uniques = lambda x: len(set([i for i in x if not math.isnan(i)]))
-users_projects_list['num_projects'] = users_projects_list['projects'].apply(num_uniques)
-
-# Loop over a certain number of users
-for index, user_projects_list in users_projects_list[users_projects_list['num_projects'] > 1].iloc[:1000].iterrows():
-    # Get the top projects
-    after_cutoff, similar_items = rec.top_projects(user_projects_list, similarity_matrix, k)
-
-    # Generate our y_true and y_pred
-    y_true, y_pred = rec.predictions(after_cutoff, similar_items)
-
-    # Evaluate our model
-    precision, recall, refined_precision = evaluate(y_true, y_pred, similar_items, similarity_matrix)
-
-    if (isinstance(precision, float) 
-            and isinstance(recall, float) 
-            and isinstance(refined_precision, float)
-            and not math.isnan(refined_precision)):
-        precisions = precisions + [precision]
-        recalls = recalls + [recall]
-        refined_precisions = refined_precisions + [refined_precision]
-
-print('--------------------------------')
-print('Model Results')
-print('Precision: ' + str(np.mean(precisions)))
-print('Recall: ' + str(np.mean(recalls)))
-print('Refined Precision: ' + str(np.mean(refined_precisions)))
-print('Max Precision: ' + str(np.max(precisions)))
-print('Max Recall: ' + str(np.max(recalls)))
-print('Max Refined Precision: ' + str(np.max(refined_precisions)))
-print('--------------------------------')
-
-
-fileName = 'cdea-results-' + str(embedding_size) + '_' + strftime("%Y-%m-%d-%H-%M-%S", gmtime()) + '.txt'
-f = open(fileName,"w+")
-for i in range(0, len(precisions)):
-    f.write('{ epoch: %s, precision: %s, recall: %s },' % (str(i), str(precisions[i]), str(recalls[i])))
-f.write('{ precision: %s, recall: %s },' % (str(np.mean(precisions)), str(np.mean(recalls))))
+# Close the results file
+f.write(']}')
 f.close()
-'''
