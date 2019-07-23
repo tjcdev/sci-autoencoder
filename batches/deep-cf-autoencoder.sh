e3a14a6468972af81c9557ad41218b600f6849ba
tjcdev <TJCARTWRIGHT91@GMAIL.COM>
2019-07-23 21:13:55 +0100

    create script for running the deep autoencoders

--- batches/deep-cf-autoencoder.sh
+++ batches/deep-cf-autoencoder.sh
@@ -5,3 +5,7 @@ screen -dm bash -c 'source activate cdea; python src/models/autoencoders/train_C
 screen -dm bash -c 'source activate cdea; python src/models/autoencoders/train_CF_autoencoder.py 1024 100 64 deep1 users_projects;'
 screen -dm bash -c 'source activate cdea; python src/models/autoencoders/train_CF_autoencoder.py 1024 100 128 deep1 users_projects;'
 
+# Train our Movies Collaborative Filtering Autoencoders
+screen -dm bash -c 'source activate cdea; python src/models/autoencoders/train_CF_autoencoder.py 1024 100 32 deep1 movies;'
+screen -dm bash -c 'source activate cdea; python src/models/autoencoders/train_CF_autoencoder.py 1024 100 64 deep1 movies;'
+screen -dm bash -c 'source activate cdea; python src/models/autoencoders/train_CF_autoencoder.py 1024 100 128 deep1 movies;'
