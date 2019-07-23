4407c8f58100144961047e376cc468d17a2175c8
tjcdev <TJCARTWRIGHT91@GMAIL.COM>
2019-07-23 12:03:25 +0100

    Update cf-autoencoder script

--- batches/cf-recommender-users-projects.sh
+++ batches/cf-recommender-users-projects.sh
@@ -2,5 +2,5 @@
 
 # CDEA autoencoder with Embedding Size 32 on UserProjects
 screen -dm bash -c 'source activate cdea; python src/experiments/run_cf_recommender_test.py 1 train_autoencoder_32_cdae_users_projects users_projects;'
-# screen -dm bash -c 'source activate cdea; python src/experiments/run_cf_recommender_test.py 5 train_autoencoder_32_cdae_users_projects users_projects;'
-# screen -dm bash -c 'source activate cdea; python src/experiments/run_cf_recommender_test.py 10 train_autoencoder_32_cdae_users_projects users_projects;'
+screen -dm bash -c 'source activate cdea; python src/experiments/run_cf_recommender_test.py 5 train_autoencoder_32_cdae_users_projects users_projects;'
+screen -dm bash -c 'source activate cdea; python src/experiments/run_cf_recommender_test.py 10 train_autoencoder_32_cdae_users_projects users_projects;'
