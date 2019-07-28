1d431679cc7d4692d3fcacb78f4d53a1598a29da
tjcdev <TJCARTWRIGHT91@GMAIL.COM>
2019-07-28 16:51:34 +0100

    Move data to bad data and fix the broken train, val, test splits and run some experiments

--- batches/cf-autoencoder.sh
+++ batches/cf-autoencoder.sh
@@ -2,6 +2,3 @@
 
 # Train our SciStarter Collaborative Filtering Autoencoders
 screen -dm bash -c 'source activate cdea; python src/models/autoencoders/train_CF_autoencoder.py 1024 100 32 cdae users_projects;'
-screen -dm bash -c 'source activate cdea; python src/models/autoencoders/train_CF_autoencoder.py 1024 100 64 cdae users_projects;'
-screen -dm bash -c 'source activate cdea; python src/models/autoencoders/train_CF_autoencoder.py 1024 100 128 cdae users_projects;'
-
--- batches/cf-recommender-users-projects.sh
+++ batches/cf-recommender-users-projects.sh
@@ -4,14 +4,3 @@
 screen -dm bash -c 'source activate cdea; python src/experiments/run_cf_recommender_test.py 1 train_autoencoder_32_cdae_users_projects users_projects;'
 screen -dm bash -c 'source activate cdea; python src/experiments/run_cf_recommender_test.py 5 train_autoencoder_32_cdae_users_projects users_projects;'
 screen -dm bash -c 'source activate cdea; python src/experiments/run_cf_recommender_test.py 10 train_autoencoder_32_cdae_users_projects users_projects;'
-
-# CDEA autoencoder with Embedding Size 64 on UserProjects
-screen -dm bash -c 'source activate cdea; python src/experiments/run_cf_recommender_test.py 1 train_autoencoder_64_cdae_users_projects users_projects;'
-screen -dm bash -c 'source activate cdea; python src/experiments/run_cf_recommender_test.py 5 train_autoencoder_64_cdae_users_projects users_projects;'
-screen -dm bash -c 'source activate cdea; python src/experiments/run_cf_recommender_test.py 10 train_autoencoder_64_cdae_users_projects users_projects;'
-
-# CDEA autoencoder with Embedding Size 128 on UserProjects
-screen -dm bash -c 'source activate cdea; python src/experiments/run_cf_recommender_test.py 1 train_autoencoder_128_cdae_users_projects users_projects;'
-screen -dm bash -c 'source activate cdea; python src/experiments/run_cf_recommender_test.py 5 train_autoencoder_128_cdae_users_projects users_projects;'
-screen -dm bash -c 'source activate cdea; python src/experiments/run_cf_recommender_test.py 10 train_autoencoder_128_cdae_users_projects users_projects;'
-
