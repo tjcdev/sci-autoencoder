97acfd7b235103691c2cbc6be350fd3bcc05a125
tjcdev <TJCARTWRIGHT91@GMAIL.COM>
2019-07-29 21:25:16 +0100

    Update user scripts

--- batches/cf-useruser.sh
+++ batches/cf-useruser.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 # Train our New-SciStarter Collaborative Filtering Autoencoders
-screen -dm bash -c 'source activate cdea; python src/experiments/run_cf_itemitem_test.py 1;'
-screen -dm bash -c 'source activate cdea; python src/experiments/run_cf_itemitem_test.py 5;'
-screen -dm bash -c 'source activate cdea; python src/experiments/run_cf_itemitem_test.py 10;'
+screen -dm bash -c 'source activate cdea; python src/experiments/run_cf_useruser_test.py 1;'
+screen -dm bash -c 'source activate cdea; python src/experiments/run_cf_useruser_test.py 5;'
+screen -dm bash -c 'source activate cdea; python src/experiments/run_cf_useruser_test.py 10;'
