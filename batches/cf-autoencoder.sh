b30c1a75926e18f93357cc40f7fe525d5e1c218f
tjcdev <TJCARTWRIGHT91@GMAIL.COM>
2019-07-29 12:55:25 +0100

    update batch file for running new scistarter experiments

--- batches/cf-autoencoder.sh
+++ batches/cf-autoencoder.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
-# Train our SciStarter Collaborative Filtering Autoencoders
-screen -dm bash -c 'source activate cdea; python src/models/autoencoders/train_CF_autoencoder.py 1024 100 32 cdae users_projects;'
+# Train our New-SciStarter Collaborative Filtering Autoencoders
+screen -dm bash -c 'source activate cdea; python src/models/autoencoders/train_CF_autoencoder.py 1024 100 32 cdae new_users_projects;'
+screen -dm bash -c 'source activate cdea; python src/models/autoencoders/train_CF_autoencoder.py 1024 100 64 cdae new_users_projects;'
+screen -dm bash -c 'source activate cdea; python src/models/autoencoders/train_CF_autoencoder.py 1024 100 128 cdae new_users_projects;'
