bf66685cdbec7a760b0e412098612f7c5f23b6bd
tjcdev <TJCARTWRIGHT91@GMAIL.COM>
2019-08-04 11:18:25 +0100

    prepare to run deep recommenders

--- batches/deep-cf-recommender-deep3-movies.sh
+++ batches/deep-cf-recommender-deep3-movies.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
-screen -dm bash -c 'source activate cdea; python src/experiments/run_cf_recommender_test.py 1 train_autoencoder_0_deep3_users_projects users_projects;'
-screen -dm bash -c 'source activate cdea; python src/experiments/run_cf_recommender_test.py 5 train_autoencoder_0_deep3_users_projects users_projects;'
-screen -dm bash -c 'source activate cdea; python src/experiments/run_cf_recommender_test.py 10 train_autoencoder_0_deep3_users_projects users_projects;'
+screen -dm bash -c 'source activate cdea; python src/experiments/run_cf_recommender_test.py 1 train_autoencoder_128_cdae_movies_0.8 movies;'
+screen -dm bash -c 'source activate cdea; python src/experiments/run_cf_recommender_test.py 5 train_autoencoder_128_cdae_movies_0.8 movies;'
+screen -dm bash -c 'source activate cdea; python src/experiments/run_cf_recommender_test.py 10 train_autoencoder_128_cdae_movies_0.8 movies;'
+
+screen -dm bash -c 'source activate cdea; python src/experiments/run_cf_recommender_test.py 1 train_autoencoder_0_deep3_movies_0.8 movies;'
+screen -dm bash -c 'source activate cdea; python src/experiments/run_cf_recommender_test.py 5 train_autoencoder_0_deep3_movies_0.8movies;'
+screen -dm bash -c 'source activate cdea; python src/experiments/run_cf_recommender_test.py 10 train_autoencoder_0_deep3_movies_0.8 movies;'
