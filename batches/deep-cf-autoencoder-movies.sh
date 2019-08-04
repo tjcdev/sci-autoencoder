67b3ee06fce039d74b748e0469e431ad4a246770
tjcdev <TJCARTWRIGHT91@GMAIL.COM>
2019-08-04 10:59:55 +0100

    remove bad experiment results

--- batches/deep-cf-autoencoder-movies.sh
+++ batches/deep-cf-autoencoder-movies.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 # Train our SciStarter Collaborative Filtering Autoencoders
-screen -dm bash -c 'source activate cdea; python src/models/autoencoders/train_CF_autoencoder.py 32 100 128 cdae new_users_projects 0.8;'
-screen -dm bash -c 'source activate cdea; python src/models/autoencoders/train_CF_autoencoder.py 32 100 0 deep3 new_users_projects 0.8;'
+screen -dm bash -c 'source activate cdea; python src/models/autoencoders/train_CF_autoencoder.py 32 100 128 cdae movies 0.8;'
+screen -dm bash -c 'source activate cdea; python src/models/autoencoders/train_CF_autoencoder.py 32 100 0 deep3 movies 0.8;'
