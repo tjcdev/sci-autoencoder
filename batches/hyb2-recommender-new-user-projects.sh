ab63f4366699036cf16f8aa3eefb845de5380d2a
tjcdev <TJCARTWRIGHT91@GMAIL.COM>
2019-08-05 11:19:02 +0100

    add field filed

--- batches/hyb2-recommender-new-user-projects.sh
+++ batches/hyb2-recommender-new-user-projects.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-screen -dm bash -c 'source activate cdea; python src/experiments/run_hyb_recommender_test.py 1 train_autoencoder_32_hyb2_new_users_projects_0.8 new_users_projects;'
-screen -dm bash -c 'source activate cdea; python src/experiments/run_hyb_recommender_test.py 1 train_autoencoder_64_hyb2_new_users_projects_0.8 new_users_projects;'
-screen -dm bash -c 'source activate cdea; python src/experiments/run_hyb_recommender_test.py 1 train_autoencoder_128_hyb2_new_users_projects_0.8 new_users_projects;'
+screen -dm bash -c 'source activate cdea; python src/experiments/run_hyb_recommender_test.py 1 train_autoencoder_32_hyb2_new_users_projects_0.8 new_users_projects description;'
+screen -dm bash -c 'source activate cdea; python src/experiments/run_hyb_recommender_test.py 1 train_autoencoder_64_hyb2_new_users_projects_0.8 new_users_projects description;'
+screen -dm bash -c 'source activate cdea; python src/experiments/run_hyb_recommender_test.py 1 train_autoencoder_128_hyb2_new_users_projects_0.8 new_users_projects description;'
 
-screen -dm bash -c 'source activate cdea; python src/experiments/run_hyb_recommender_test.py 5 train_autoencoder_32_hyb2_new_users_projects_0.8 new_users_projects;'
-screen -dm bash -c 'source activate cdea; python src/experiments/run_hyb_recommender_test.py 5 train_autoencoder_64_hyb2_new_users_projects_0.8 new_users_projects;'
-screen -dm bash -c 'source activate cdea; python src/experiments/run_hyb_recommender_test.py 5 train_autoencoder_128_hyb2_new_users_projects_0.8 new_users_projects;'
+screen -dm bash -c 'source activate cdea; python src/experiments/run_hyb_recommender_test.py 5 train_autoencoder_32_hyb2_new_users_projects_0.8 new_users_projects description;'
+screen -dm bash -c 'source activate cdea; python src/experiments/run_hyb_recommender_test.py 5 train_autoencoder_64_hyb2_new_users_projects_0.8 new_users_projects description;'
+screen -dm bash -c 'source activate cdea; python src/experiments/run_hyb_recommender_test.py 5 train_autoencoder_128_hyb2_new_users_projects_0.8 new_users_projects description;'
 
-screen -dm bash -c 'source activate cdea; python src/experiments/run_hyb_recommender_test.py 10 train_autoencoder_32_hyb2_new_users_projects_0.8 new_users_projects;'
-screen -dm bash -c 'source activate cdea; python src/experiments/run_hyb_recommender_test.py 10 train_autoencoder_64_hyb2_new_users_projects_0.8 new_users_projects;'
-screen -dm bash -c 'source activate cdea; python src/experiments/run_hyb_recommender_test.py 10 train_autoencoder_128_hyb2_new_users_projects_0.8 new_users_projects;'
+screen -dm bash -c 'source activate cdea; python src/experiments/run_hyb_recommender_test.py 10 train_autoencoder_32_hyb2_new_users_projects_0.8 new_users_projects description;'
+screen -dm bash -c 'source activate cdea; python src/experiments/run_hyb_recommender_test.py 10 train_autoencoder_64_hyb2_new_users_projects_0.8 new_users_projects description;'
+screen -dm bash -c 'source activate cdea; python src/experiments/run_hyb_recommender_test.py 10 train_autoencoder_128_hyb2_new_users_projects_0.8 new_users_projects description;'
