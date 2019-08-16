7dd04a49ed2c2923ac5ee061d8c17c246aa0357c
tjcdev <TJCARTWRIGHT91@GMAIL.COM>
2019-08-15 18:02:46 -0700

    prepare batch files to run

--- data/data_models/content_data.py
+++ data/data_models/content_data.py
@@ -16,32 +16,6 @@ from keras.utils.data_utils import get_file
 from keras.utils.np_utils import to_categorical
 from zipfile import ZipFile
 
-def load_projects_tfidf(field):
-    # Load the full project data from the pickle file
-    projects = pd.read_pickle("data/processed/project_data")
-
-    # Get the TF-IDF for the description fields
-    v = TfidfVectorizer(max_features=3000)
-    desc_idf = v.fit_transform(projects[field])
-
-    # Train/Val/Test Split
-    test_split_idx = int(np.floor(desc_idf.shape[0] * 0.8))
-    val_split_idx = int(test_split_idx * 0.9)
-
-    train_x = desc_idf[:val_split_idx]
-    val_x = desc_idf[val_split_idx:test_split_idx]
-    test_x = desc_idf[test_split_idx:]
-
-    train_labels_idx = np.arange(0, val_split_idx)
-    val_labels_idx = np.arange(val_split_idx, test_split_idx)
-    test_labels_idx = np.arange(test_split_idx, desc_idf.shape[0])
-
-    train_labels = pd.DataFrame(projects['project_id'].iloc[:val_split_idx], index=train_labels_idx)
-    val_labels = pd.DataFrame(projects['project_id'].iloc[val_split_idx:test_split_idx], index=val_labels_idx)
-    test_labels = pd.DataFrame(projects['project_id'].iloc[test_split_idx:], index=test_labels_idx)
-
-    return train_labels, train_x, val_labels, val_x, test_labels, test_x
-
 def load_cf_projects_tfidf(field):
     # Load the full project data from the pickle file
     content_projects = pd.read_pickle("data/processed/cf_projects_data")
@@ -70,7 +44,7 @@ def load_cf_projects_tfidf(field):
 
 def load_cf_projects_doc2vec(field):
     # Load the full project data from the pickle file
-    projects = pd.read_pickle("data/processed/project_data")
+    projects = pd.read_pickle("data/processed/cf_projects_data")
 
     # Get the Doc2Vec for the description fields
     # Transform data (you can add more data preprocessing steps) 
